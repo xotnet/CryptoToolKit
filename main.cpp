e7c99266ce0026345f44024329ae93dce6ddbdf2
xotnet <150250186+xotnet@users.noreply.github.com>
2024-02-25 09:11:54 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-// Encryption ToolKit
 #include <iostream>
 #include <bitset>
 #include <fstream>
@@ -8,114 +7,30 @@ void swapBits(int &bit) {
 	else if (bit == 0) {bit = 1;}
 }
 std::string toBitAndEncrypt(std::string inputString, std::string key) {
-	std::string preBin = "";
-	std::string cryptedBin = "";
-    for (char c : inputString) {
-        std::bitset<8> bits(c); // bitset is container with size 8
-		preBin += bits.to_string();
-    }
-	std::string finalBin;
-	int i = 0;
-	while (i < preBin.length()) {
-		int binInt = preBin[i] - '0';
-		int cleanBinInt = binInt;
-		int s = 0;
-		while (s < key.length()) { // [ENCRYPTION] encrypting every byte
-			std::bitset<8> bits(key[s]);
-			int g = 0;
-			while (cleanBinInt == binInt) {
-				swapBits(binInt);
-				if (bits[0] == binInt) {swapBits(binInt);}
-				if (bits[1] == binInt) {swapBits(binInt);}
-				if (bits[2] == binInt) {swapBits(binInt);}
-				if (bits[3] == binInt) {swapBits(binInt);}
-				if (bits[4] == binInt) {swapBits(binInt);}
-				if (bits[5] == binInt) {swapBits(binInt);}
-				if (bits[6] == binInt) {swapBits(binInt);}
-				if (bits[7] == binInt) {swapBits(binInt);}
-				
-				if (bits[g] == 0) {bits[g] = 1;}
-				else if (bits[g] == 1) {bits[g] = 0;}
-				
-				if (g < 8) {g++;}
-				else {g = 0;}
-			}
-			s++; // next key char
-		}
-		finalBin = finalBin + std::to_string(binInt);
-		i++; // next bit
-	}
 	std::string toChars8bit = "";
-	i = 0;
-	int u = 0;
-    while (i*8 <= finalBin.length()){
-		std::string binarySequence = "";
-		binarySequence = "";
-		while (u < i*8) {
-			binarySequence += finalBin[u];
-			u++;
+	std::string preBin = "";
+	for (char c : inputString) { // every char in message
+		std::bitset<8> bits(c); // bitset is container with size 8
+		for (int u = 0; u < key.length(); u++) { // bitset in key char
+			std::bitset<8> keyCharToBit(key[u]);
+			bits = bits ^ keyCharToBit;
 		}
-        std::bitset<8> bits(binarySequence); // bitset is container with size 8
-		toChars8bit += static_cast<char>(bits.to_ulong());
-		i++;
-    }
+		toChars8bit = toChars8bit + static_cast<char>(bits.to_ulong());
+	}
 	return toChars8bit;
 }
 
 std::string toStringAndDecrypt(std::string inputString, std::string key) {
-	std::string finalBin = "";
-	std::string preBin = "";
-	std::string toBit8 = "";
-	int i = 0;
-	for (char c : inputString) {
-        std::bitset<8> bits(c); // bitset is container with size 8
-		toBit8 = toBit8 + bits.to_string();
-		i++;
-    }
-	int o = 0;
-	while (o < toBit8.length()) {
-		int binInt = toBit8[o] - '0';
-		int s = 0;
-		int cleanBinInt = binInt;
-		while (s < key.length()) { // decrypting every byte
-			std::bitset<8> bits(key[s]);
-			int g = 0;
-			while (cleanBinInt == binInt) {
-				swapBits(binInt);
-				if (bits[0] == binInt) {swapBits(binInt);}
-				if (bits[1] == binInt) {swapBits(binInt);}
-				if (bits[2] == binInt) {swapBits(binInt);}
-				if (bits[3] == binInt) {swapBits(binInt);}
-				if (bits[4] == binInt) {swapBits(binInt);}
-				if (bits[5] == binInt) {swapBits(binInt);}
-				if (bits[6] == binInt) {swapBits(binInt);}
-				if (bits[7] == binInt) {swapBits(binInt);}
-				
-				if (bits[g] == 0) {bits[g] = 1;}
-				else if (bits[g] == 1) {bits[g] = 0;}
-				
-				if (g < 8) {g++;}
-				else {g = 0;}
-			}
-			s++; // next key char
+	std::string toCharFromBin = "";
+	for (int i = 0; i < inputString.length(); i++) { // every char in encrypted message
+		std::bitset<8> bits(inputString[i]); // convert char to bitset
+		for (int u = 0; u < key.length(); u++) { // bitset in key char
+			std::bitset<8> keyCharToBit(key[u]);
+			bits = bits ^ keyCharToBit;
 		}
-		preBin = preBin + std::to_string(binInt);
-		o++;
+		toCharFromBin = toCharFromBin + static_cast<char>(bits.to_ulong());
 	}
-	i = 1;
-	int u = 0;
-    while (i*8 <= preBin.length()){
-		std::string binarySequence = "";
-		binarySequence = "";
-		while (u < i*8) {
-			binarySequence += preBin[u];
-			u++;
-		}
-        std::bitset<8> bits(binarySequence); // bitset is container with size 8
-		finalBin += static_cast<char>(bits.to_ulong());
-		i++;
-    }
-	return finalBin;
+	return toCharFromBin;
 }
 
 void cryptFile (std::string path, std::string key) {
@@ -157,7 +72,7 @@ int main() {
 	short int selectDecEnc;
 	short int selectTarget;
 	std::string key;
-	std::cout << "Select target:\n[1] Message\n[2] File\n[3] Generate key (Soon)\n: ";
+	std::cout << "Select target:\n[1] Message\n[2] File\n[3] Generate key\n: ";
 	std::cin >> selectTarget;
 	if (selectTarget != 3) {
 		std::cout << "Select method:\n[1] Encrypt\n[2] Decrypt\n: ";
@@ -172,7 +87,7 @@ int main() {
 		std::cout << "String to encrypt: ";
 		std::cin.ignore();
 		std::getline(std::cin, inputString);
-		std::cout << "\nEncrypted message:" << toBitAndEncrypt(inputString, key) << "\n";
+		std::cout << "\nEncrypted message: '" << toBitAndEncrypt(inputString, key) << "'\n";
 		system("pause");
 	}
 	else if (selectDecEnc == 2 && selectTarget == 1){
